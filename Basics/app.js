97959a6503af4884d896912d67b439fc8c56fc2b
Gerson Jean Pierre <iblezya@gmail.com>
2022-01-24 14:12:23 -0500

    Introduction

---
+++ Basics/app.js
@@ -0,0 +1,8 @@
+"use strict";
+const message = 'Hola crag de crags';
+console.log(message);
+/*
+tsc : TypeScript compiler
+tsc --init : create tsconfig.json
+tsc --watch : observer mode
+*/ 
---
+++ Basics/app.ts
@@ -0,0 +1,11 @@
+
+
+const message: string = 'Hola crag de crags';
+
+console.log(message);
+
+/* 
+tsc : TypeScript compiler
+tsc --init : create tsconfig.json
+tsc --watch : observer mode
+*/
