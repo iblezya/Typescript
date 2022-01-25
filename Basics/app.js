a8f1e48b432eefd21e46abc0a96a15816dd7bc4f
Gerson Jean Pierre <iblezya@gmail.com>
2022-01-25 13:48:39 -0500

    Homework

--- Basics/app.js
+++ Basics/app.js
@@ -1,15 +1,35 @@
 "use strict";
-/*
-tsc : TypeScript compiler
-tsc --init : create tsconfig.json
-tsc --watch : observer mode
-*/
-// Anonymous Functions
 (() => {
-    const a = 10;
-    let b;
-    function sayHello(msg) {
-        console.log(msg);
-    }
-    sayHello('Hola crag crag');
+    // Tipos
+    const batman = 'Bruce';
+    const superman = 'Clark';
+    const existe = false;
+    // Tuplas
+    const parejaHeroes = [batman, superman];
+    const villano = ['Lex Lutor', 5, true];
+    // Arreglos
+    const aliados = ['Mujer Maravilla', 'Acuaman', 'San', 'Flash'];
+    //Enumeraciones
+    let HeroStr;
+    (function (HeroStr) {
+        HeroStr[HeroStr["Acuaman"] = 0] = "Acuaman";
+        HeroStr[HeroStr["Batman"] = 1] = "Batman";
+        HeroStr[HeroStr["Flash"] = 5] = "Flash";
+        HeroStr[HeroStr["Superman"] = 100] = "Superman";
+    })(HeroStr || (HeroStr = {}));
+    const fuerzaAcuaman = HeroStr.Acuaman;
+    const fuerzaBatman = HeroStr.Batman;
+    const fuerzaFlash = HeroStr.Flash;
+    const fuerzaSuperman = HeroStr.Superman;
+    // Retorno de funciones
+    const activar_batiseñal = () => {
+        return 'activada';
+    };
+    const pedir_ayuda = () => {
+        console.log('Auxilio!!!');
+    };
+    // Aserciones de Tipo
+    const poder = '100';
+    const largoDelPoder = poder.length;
+    console.log(largoDelPoder);
 })();
--- Basics/app.ts
+++ Basics/app.ts
@@ -1,16 +1,48 @@
-/* 
-tsc : TypeScript compiler
-tsc --init : create tsconfig.json
-tsc --watch : observer mode
-*/
-
-// Anonymous Functions
 (() => {
-  const a:number = 10;
-  let b;
 
-  function sayHello (msg:string) {
-    console.log(msg)
-  }
-  sayHello('Hola crag crag')
-})()
+    // Tipos
+    const batman: string = 'Bruce';
+    const superman: string = 'Clark';
+  
+    const existe: boolean = false;
+  
+    // Tuplas
+    const parejaHeroes: [string, string] = [batman,superman];
+    const villano: [ string, number, boolean ] = ['Lex Lutor',5,true];
+  
+    // Arreglos
+    const aliados: string[] = ['Mujer Maravilla','Acuaman','San', 'Flash'];
+  
+    //Enumeraciones
+    enum HeroStr {
+      Acuaman = 0,
+      Batman = 1,
+      Flash = 5,
+      Superman = 100,
+    }
+
+    const fuerzaAcuaman: HeroStr = HeroStr.Acuaman
+    const fuerzaBatman: HeroStr = HeroStr.Batman
+    const fuerzaFlash: HeroStr = HeroStr.Flash
+    const fuerzaSuperman: HeroStr = HeroStr.Superman
+  
+    // Retorno de funciones
+    const activar_batiseñal= (): string => {
+      return 'activada';
+    }
+
+
+  
+    const pedir_ayuda = (): void => {
+      console.log('Auxilio!!!');
+    }
+  
+    // Aserciones de Tipo
+    const poder: any = '100';
+    const largoDelPoder:number = (poder as string).length;
+    console.log( largoDelPoder );
+  
+  
+  })()
+  
+  
