fb15a310602dcbc19c92cbe8f835eb55f225ea80
Gerson Jean Pierre <iblezya@gmail.com>
2022-02-02 13:09:11 -0500

    Classes: Get and Set

--- Basics/classes/extends.ts
+++ Basics/classes/extends.ts
@@ -24,14 +24,30 @@
       console.log('Constructor AntiHero llamado!')
     }
 
+    
+    public get fullName() : string {
+      return `${ this.name } - ${ this.team }`
+    }
+    
+    
+    public set fullName(name : string) {
+      if ( name.length < 3 ) {
+        throw new Error('El nombre debe ser mayor de 3 letras.')
+      }
+
+      this.name = name;
+    }
+    
+
     getFullNameDesdeAntiHero() {
       console.log( super.getFullName() )
     }
 
   }
 
-  const doom = new AntiHero('Doom','Dire', true)
-  console.log(doom)
-  doom.getFullNameDesdeAntiHero()
-
+  //const doom = new AntiHero('Doom','Dire', true)
+  // console.log(doom.fullName)
+  // doom.getFullNameDesdeAntiHero()
+  // doom.fullName = 'Blez'
+  // console.log(doom.fullName)
 })()
