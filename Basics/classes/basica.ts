591fde4913f45ad1ef1d5495e11ad64bcb54b31c
Gerson Jean Pierre <iblezya@gmail.com>
2022-02-02 00:42:11 -0500

    Class public and private methods

--- Basics/classes/basica.ts
+++ Basics/classes/basica.ts
@@ -6,6 +6,9 @@
     //public team: string;
     //public power: number;
     static avgAge: number = 40;
+    static getNameClass() {
+      return this.name;
+    }
 
     constructor( 
       private name: string, 
@@ -13,10 +16,15 @@
       public power?: number 
     ) {}
 
+    public bio(){
+      return `${ this.name } (${ this.team })`
+    }
+
   }
 
   const slark: Hero = new Hero( 'Slark', 'Dire', 5000 );
   console.log( slark )
-  console.log( Hero.avgAge )
+  console.log( slark.bio() )
+  console.log( Hero.getNameClass() )
 
 })()
