355c2022e8a95462fdf88d3e635ddd79d5318db8
Gerson Jean Pierre <iblezya@gmail.com>
2022-02-02 19:33:42 -0500

    Interfaces: Basic Interface

---
+++ Basics/interfaces/basic.ts
@@ -0,0 +1,25 @@
+(() => {
+
+  interface Hero {
+    name: string,
+    age?: number,
+    skills: string[],
+    getName?: () => string
+  }
+
+  let slark: Hero = {
+    name: 'Slark',
+    age: 23,
+    skills: ['Shadow']
+  }
+  let sven: Hero = {
+    name: 'Slark',
+    age: 30,
+    skills: ['Hammer','God Str']
+  }
+  
+  
+
+  
+
+})()
