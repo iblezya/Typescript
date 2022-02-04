2561f65df74685f28f9ae6c9a3fc966e8f919462
Gerson Jean Pierre <iblezya@gmail.com>
2022-02-04 15:41:47 -0500

    Interfaces: interfaces for functions

---
+++ Basics/interfaces/function.ts
@@ -0,0 +1,14 @@
+(() => {
+
+  // Muy rara vez usado
+  interface addTwoNumbers {
+    (a: number, b: number): number;
+  }
+
+  let addNumbersFunction: addTwoNumbers;
+
+  addNumbersFunction = (a: number, b: number) => {
+    return 10;
+  }
+
+})()
