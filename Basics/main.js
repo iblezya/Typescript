8b0460aaa0138d38ece680496763e6e0f3b13d33
Gerson Jean Pierre <iblezya@gmail.com>
2022-02-05 04:23:39 -0500

    Namespaces

--- Basics/main.js
+++ Basics/main.js
@@ -1,40 +1,12 @@
 "use strict";
-var __rest = (this && this.__rest) || function (s, e) {
-    var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
-        t[p] = s[p];
-    if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
-            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
-                t[p[i]] = s[p[i]];
-        }
-    return t;
-};
-(() => {
-    const names = {
-        blez: 'Gerson',
-        take: 'Jean',
-        bazik: 'Pierre',
-        activo: true,
-        poder: 1500.4456
+var Validations;
+(function (Validations) {
+    Validations.validateText = (text) => {
+        return (text.length > 3) ? true : false;
     };
-    const printName = (names) => {
-        console.log(names.bazik);
+    Validations.validateDate = (myDate) => {
+        return (isNaN(myDate.valueOf())) ? false : true;
     };
-    const printName2 = (_a) => {
-        var { blez } = _a, resto = __rest(_a, ["blez"]);
-        console.log(blez, resto);
-    };
-    printName(names);
-    printName2(names);
-    const namesArr = ['Raul', 'Saul', 'Boul'];
-    const namesArr2 = ['Raul', false, 147.5111];
-    const [ral, blou, numero] = namesArr2;
-})();
-(() => {
-    const name = 'Blez';
-    const getName = () => {
-        console.log('Hola crags');
-    };
-})();
+})(Validations || (Validations = {}));
+console.log(Validations.validateText('BLez'));
 //# sourceMappingURL=main.js.map
