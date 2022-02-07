84d26fca56af0a21912431ef8d0fbdbff28983a7
Gerson Jean Pierre <iblezya@gmail.com>
2022-02-07 12:53:20 -0500

    Module ande Webpack, Imports and Exports

---
+++ ts-node/src/classes/Hero.ts
@@ -0,0 +1,11 @@
+
+
+export class Hero {
+  constructor(
+    public name: string,
+    public powerId: number,
+    public age: number
+  ) {
+    
+  }
+}
---
+++ ts-node/src/index.ts
@@ -0,0 +1,12 @@
+import { Hero } from './classes/Hero';
+
+
+console.log('Hola Mundo!');
+
+
+const slark = new Hero('Slark',1,30)
+
+console.log(slark)
+
+
+
