e1a668ac3b5f248a49a52529f19c64a1104b7620
Gerson Jean Pierre <iblezya@gmail.com>
2022-02-07 23:18:57 -0500

    Generic Function

--- ts-node/src/generics/generics.ts
+++ ts-node/src/generics/generics.ts
@@ -3,6 +3,8 @@ export const printObject = ( argument: any ) => {
   console.log( argument );
 }
 
-export function generciFunction( argument: any ) {
+export function generciFunction<T>( argument: T ) {
   return argument;
 }
+
+export const genericFunctionArrow = <T>( argument: T) => argument;
--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,4 +1,4 @@
-import { printObject, generciFunction } from './generics/generics';
+import { printObject, generciFunction, genericFunctionArrow } from './generics/generics';
 
 
 // printObject('Hola crags');
@@ -6,4 +6,5 @@ import { printObject, generciFunction } from './generics/generics';
 // printObject( {A:1, B:2, C:3} )
 
 console.log( generciFunction(3.1416).toFixed(2) )
-console.log( generciFunction( new Date() ).toFixed(2) )
+console.log( generciFunction( new Date() ).getDate() )
+console.log( genericFunctionArrow( new Date() ).getDate() )
